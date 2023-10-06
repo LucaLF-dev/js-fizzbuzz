5123f7f3c65158fa692975418adce64db736532b
LucaLF-dev <laficaraluca@gmail.com>
2023-10-06 17:24:45 +0200

    Added - readme and js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+// - creiamo un ciclo che stampi i numeri da 1 a 100
+for (let i = 0; i < 100; i++) {
+    let n = i + 1;
+    // console.log(n);
+
+// - creo una varibile resto per i multipli di tre:
+    const resto = n % 3
+    console.log(n, resto)
+//   - SE sono multipli di 3 e quindi il resto è 0 stampo FIZZ
+if (resto === 0){
+    console.log('fizz')
+    
+}
+// - creo una variabile resto per i multipli di 5:
+//   - SE sono multipli di 5 e quindi il resto è 0 stampo BUZZ
+
+    
+}
